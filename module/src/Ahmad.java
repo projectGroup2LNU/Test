0fdeb24dc9e272524176298b042bb17a766caa28
AhmadVakil <av222gx@student.lnu.se>
2017-03-23 09:08:57 -0700

    Update Ahmad.java
    Here is the basic new window, with JMenu and other related MenuItems,
    Each menu will follow by other menu and categorize the user request as much as possible to be as clear as possible.
    New events:
    Will ask the type of events and will open new smaller window to ask the user for duration and time

--- module/src/Ahmad.java
+++ module/src/Ahmad.java
@@ -1,4 +1,75 @@
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+
 public class Ahmad
 {
+  private JFrame frmTitleMethod;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ahmad window = new Ahmad();
+					window.frmTitleMethod.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ahmad() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmTitleMethod = new JFrame();
+		frmTitleMethod.setTitle("Title Method();");
+		frmTitleMethod.setResizable(false);
+		frmTitleMethod.setBounds(100, 100, 1399, 903);
+		frmTitleMethod.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTitleMethod.getContentPane().setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 1393, 26);
+		frmTitleMethod.getContentPane().add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Events");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewEvent = new JMenu("New Event");
+		mnNewMenu.add(mnNewEvent);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New Event with duration");
+		mnNewEvent.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewEventWithout = new JMenuItem("New Event without duration");
+		mnNewEvent.add(mntmNewEventWithout);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmGetStarted = new JMenuItem("Get started");
+		mnHelp.add(mntmGetStarted);
+		
+		JMenuItem mntmAboutUs = new JMenuItem("About us");
+		mnHelp.add(mntmAboutUs);
+		
+		JMenuItem mntmContactUs = new JMenuItem("Contact us");
+		mnHelp.add(mntmContactUs);
+	}
 }
